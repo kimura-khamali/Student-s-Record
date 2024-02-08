77de3cb19956db984c50c7c86b8d2954c997b008
kimura-khamali <khamalikimura@gmail.com>
2024-02-08 22:51:40 +0300

    added file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main () {
+    val student: String = "Jane"
+    val  age: Int = 20
+    val phoneNumber: Int = 745689476
+    val weight: Int = 70
+    val kenyanCitizen= true
+    val notKenyanCitizen= false
+
+    println(student)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(kenyanCitizen)
+    println(notKenyanCitizen)
+    println(message="$student records,she is $age years old.This is her phone number $phoneNumber.She has a weight of $weight kg and she's a $kenyanCitizen Kenyan citizen.")
+    //println(message="$student records,she is $age years old.This is her phone number $phoneNumber.She has a weight of $weight kg and she's a $notKenyanCitizen Kenyan citizen.")
+}
